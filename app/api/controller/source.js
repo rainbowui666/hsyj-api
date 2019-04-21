f10386e59ca49baff6df52298606fd4bcfd9799f
rainbowui666 <zhoujielun@163.com>
2019-04-21 20:43:18 +0800

    update api

--- app/api/controller/source.js
+++ app/api/controller/source.js
@@ -32,5 +32,30 @@ module.exports = class extends Base {
             return _this2.success('删除成功');
         })();
     }
+
+    getListBytargetidAndSourceTypeAction() {
+        var _this3 = this;
+
+        return _asyncToGenerator(function* () {
+            const targetid = _this3.get('targetid');
+            const sourcetype = _this3.get('sourcetype');
+            const pageindex = _this3.get('pageindex') || 1;
+            const pagesize = _this3.get('pagesize') || 10;
+
+            const model = _this3.model('source');
+            model._pk = 'sourceID';
+
+            let para = {};
+            if (!think.isEmpty(targetid) && !think.isEmpty(sourcetype)) {
+                para = { targetid: targetid, sourceType: sourcetype };
+            } else if (!think.isEmpty(targetid)) {
+                para = { targetid: targetid };
+            } else {
+                para = { sourceType: sourcetype };
+            }
+            const data = yield model.where(para).page(pageindex, pagesize).countSelect();
+            return _this3.success(data);
+        })();
+    }
 };
 //# sourceMappingURL=source.js.map
--- src/api/controller/source.js
+++ src/api/controller/source.js
@@ -22,4 +22,25 @@ module.exports = class extends Base {
         const date = await this.model('source').where({sourceID:id}).delete();
         return this.success('删除成功')
     }
+
+    async getListBytargetidAndSourceTypeAction(){
+        const targetid = this.get('targetid');
+        const sourcetype = this.get('sourcetype');
+        const pageindex = this.get('pageindex') || 1;
+        const pagesize = this.get('pagesize') || 10;
+
+        const model = this.model('source');
+        model._pk = 'sourceID';
+
+        let para = { };
+        if (!think.isEmpty(targetid) && !think.isEmpty(sourcetype)) {
+            para = {targetid:targetid, sourceType:sourcetype}
+        } else if(!think.isEmpty(targetid)) {
+            para = {targetid:targetid}
+        } else {
+            para = {sourceType:sourcetype}
+        }
+        const data = await model.where(para).page(pageindex, pagesize).countSelect();
+        return this.success(data)
+    }
 }
