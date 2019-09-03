a3ed48cb2cc4002d86362686201334f0442b44b4
jyhs <jyhs_123@163.com>
2019-09-03 22:41:39 +0800

    update

--- src/api/controller/count.js
+++ src/api/controller/count.js
@@ -1,14 +1,21 @@
 const Base = require('./base.js');
+const _ = require('lodash');
 
 module.exports = class extends Base {
     async getHotMapDataAction() {
         const id = this.get('id');
-        const scencerys = await this.model("activity_scenery").field('sceneryid').where({activityid:id}).select();
-        const sql = `select sceneryid,count(1) num from culture_student_scenery where sceneryid in (${scencerys.join(",")}) to_days(createdate) = to_days(now()) group by sceneryid`;
+        // const scencerys = await this.model("activity_scenery").field(['sceneryid']).where({activityid:id}).select();
+        // const ids = [];
+        // _.each(scencerys,(s)=>{
+        //     ids.push(s.sceneryid);
+        // });
+        // const sql = `select sceneryid,count(1) num from culture_student_scenery where sceneryid in (${ids.join(",")}) and to_days('createdate') = to_days(now()) group by sceneryid`;
+        const sql = `select sceneryid,count(1) num from culture_student_scenery where activityid=${id} group by sceneryid`;
 
         // const sql = `select sceneryid,count(1) num from culture_student_scenery  group by sceneryid`
         const counts = await this.model('student_scenery').query(sql);
-        const scencry = await this.model('scenery').where({sceneryID:scencerys[0].sceneryid}).find();
+        const where = counts[0]?{sceneryID:counts[0].sceneryid}:{sceneryID:0}
+        const scencry = await this.model('scenery').where(where).find();
         const hotdata = {
             data:[],
             lng:scencry.longitude,
